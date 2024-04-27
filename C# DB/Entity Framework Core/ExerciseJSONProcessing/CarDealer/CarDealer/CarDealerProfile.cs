94ed90299055cb8142d62ba031896693f8ad8cfc
Borislav16 <borko.g16@gmail.com>
2024-04-27 09:37:16 +0300

    Create CarDealerProfile.cs

---
+++ C# DB/Entity Framework Core/ExerciseJSONProcessing/CarDealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<SupplierDTO, Supplier>();
+            CreateMap<PartDTO, Part>();
+            this.CreateMap<CarDTO, Car>();
+        }
+    }
+}
