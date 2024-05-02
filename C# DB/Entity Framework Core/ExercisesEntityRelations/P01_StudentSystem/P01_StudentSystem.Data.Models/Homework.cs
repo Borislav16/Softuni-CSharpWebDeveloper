7c2ccce7a95b6e517adb32c39b4aecca3a902282
Borislav16 <borko.g16@gmail.com>
2024-05-02 15:15:41 +0300

    Create Homework.cs

---
+++ C# DB/Entity Framework Core/ExercisesEntityRelations/P01_StudentSystem/P01_StudentSystem.Data.Models/Homework.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+
+        [Required]
+        [Unicode(false)]
+        public string Content { get; set; }
+
+        [Required]
+        public ContentType ContentType { get; set; }
+        [Required]
+        public DateTime SubmissionTime { get; set; }
+        [Required]
+        public int StudentId { get; set; }
+
+        [ForeignKey(nameof(StudentId))]
+        public virtual Student Student { get; set; }
+        [Required]
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public virtual Course Course { get; set; }
+    }
+}
