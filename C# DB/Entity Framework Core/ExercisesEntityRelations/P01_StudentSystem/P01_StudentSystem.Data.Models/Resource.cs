f04ba6fd73d9d92e0288c7e0ef0e8a4a77831dac
Borislav16 <borko.g16@gmail.com>
2024-05-02 15:16:15 +0300

    Create Resource.cs

---
+++ C# DB/Entity Framework Core/ExercisesEntityRelations/P01_StudentSystem/P01_StudentSystem.Data.Models/Resource.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace P01_StudentSystem.Data.Models
+{
+
+    public class Resource
+    {
+        
+        public int ResourceId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public ResourceType ResourceType { get; set; }
+
+       
+        public int CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public virtual Course Course { get; set; }
+    }
+}
