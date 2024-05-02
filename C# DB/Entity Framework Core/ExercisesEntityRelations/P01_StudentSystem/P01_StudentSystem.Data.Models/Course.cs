a3aaca96c58e8e652dd7eb75cb52af8b9e502eb3
Borislav16 <borko.g16@gmail.com>
2024-05-02 15:15:33 +0300

    Create Course.cs

---
+++ C# DB/Entity Framework Core/ExercisesEntityRelations/P01_StudentSystem/P01_StudentSystem.Data.Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        
+
+        [Key]
+        public int CourseId { get; set; }
+
+        
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        [Unicode(true)]
+        public string? Description { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Homework> Homeworks { get; set; }
+        public virtual ICollection<Resource> Resources { get; set; }
+        public virtual ICollection<StudentCourse> StudentsCourses { get; set; }
+    }
+}
