d8f2a699a2ce77b63dee13735433271ca83d8694
Borislav16 <borko.g16@gmail.com>
2024-05-14 12:09:09 +0300

    Create HomeController.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation/Library/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Library.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if(User.Identity.IsAuthenticated == true)
+            {
+                return RedirectToAction("All", "Book");
+            }
+            return View();
+        }
+
+
+    }
+}
