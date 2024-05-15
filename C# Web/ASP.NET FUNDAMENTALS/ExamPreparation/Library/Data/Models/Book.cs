636d8751c90fa0c91c16a69ec19c38ad85d93428
Borislav16 <borko.g16@gmail.com>
2024-05-15 13:05:28 +0300

    Create Book.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation/Library/Data/Models/Book.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Library.Data.DataConstants.Book;
+namespace Library.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(AuthorMaxLength)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        public List<IdentityUserBook> UsersBooks { get; set; } = new List<IdentityUserBook>();
+    }
+}
