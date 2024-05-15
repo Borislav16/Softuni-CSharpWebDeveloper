0433241ed829722b5a35aff7f27bb773c99bd1b1
Borislav16 <borko.g16@gmail.com>
2024-05-15 13:03:58 +0300

    Create DataConstants.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation/Library/Data/DataConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace Library.Data
+{
+    public static class DataConstants
+    {
+        public static class Book
+        {
+            public const int TitleMaxLength = 50;
+            public const int TitleMinLength = 10;
+
+            public const int AuthorMaxLength = 50;
+            public const int AuthorMinLength = 5;
+
+            public const int DescriptionMaxLength = 5000;
+            public const int DescriptionMinLength = 5;
+
+            public const double RatingMaxValue = 10;
+            public const double RatingMinValue = 0;
+        }
+
+        public static class Category
+        {
+            public const int NameMaxLength = 50;
+            public const int NameMinLength = 5;
+        }
+    }
+}
