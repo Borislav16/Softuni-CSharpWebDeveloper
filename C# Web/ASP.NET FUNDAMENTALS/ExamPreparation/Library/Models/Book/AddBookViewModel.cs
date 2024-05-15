a6990ea73966be4aa280cf0015b5efbb20c685b9
Borislav16 <borko.g16@gmail.com>
2024-05-15 13:05:58 +0300

    Create AddBookViewModel.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation/Library/Models/Book/AddBookViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Library.Data.Models;
+using Library.Models.Caategory;
+using static Library.Data.DataConstants.Book;
+
+namespace Library.Models.Book
+{
+    public class AddBookViewModel
+    {
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(AuthorMaxLength, MinimumLength = AuthorMinLength)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+        
+        [Required]
+        public string Url { get; set; } = null!;
+
+        [Required]
+        [Range(0.00, 10.00)]
+        public decimal Rating { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public List<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+    }
+}
