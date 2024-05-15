2ecc0726655b2b17e1664b41977cd7d42f88e45a
Borislav16 <borko.g16@gmail.com>
2024-05-15 13:06:15 +0300

    Create BookViewModel.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation/Library/Models/Book/BookViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Library.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static Library.Data.DataConstants.Book;
+namespace Library.Models.Book
+{
+    public class BookViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(AuthorMaxLength, MinimumLength = AuthorMinLength)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [Range(0.00, 10.00)]
+        public decimal Rating { get; set; }
+
+        [Required]
+        public string Category { get; set; } = null!;
+    }
+}
