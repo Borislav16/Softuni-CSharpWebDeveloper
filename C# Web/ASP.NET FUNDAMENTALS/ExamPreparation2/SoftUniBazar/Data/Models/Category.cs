d5111eedde268062eb34bf3c95feecd2e6c7e6c7
Borislav16 <borko.g16@gmail.com>
2024-05-20 14:24:44 +0300

    Create Category.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation2/SoftUniBazar/Data/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SoftUniBazar.Data.Constants.DataConstants.Category;
+
+namespace SoftUniBazar.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public List<Ad> Ads { get; set; } = new List<Ad>();
+    }
+}
