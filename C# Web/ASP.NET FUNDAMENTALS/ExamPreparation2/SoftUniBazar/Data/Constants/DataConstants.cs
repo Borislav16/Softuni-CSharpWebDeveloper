11a64c59b9915eefb02a6f30b26ba471ff48c947
Borislav16 <borko.g16@gmail.com>
2024-05-20 14:22:56 +0300

    Create DataConstants.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation2/SoftUniBazar/Data/Constants/DataConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace SoftUniBazar.Data.Constants
+{
+    public static class DataConstants
+    {
+        public static class Ad
+        {
+            public const int NameMaxLegth = 25;
+            public const int NameMinLegth = 5;
+
+            public const int DescriptionMaxLegth = 250;
+            public const int DescriptionMinLegth = 15;
+        }
+
+        public static class Category
+        {
+            public const int NameMaxLength = 15; 
+            public const int NameMinLength = 3; 
+        }
+    }
+}
