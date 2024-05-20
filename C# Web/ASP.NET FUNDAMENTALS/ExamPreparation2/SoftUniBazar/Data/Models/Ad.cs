1597485f76106aaaacb26127793c51dd42e51943
Borislav16 <borko.g16@gmail.com>
2024-05-20 14:24:27 +0300

    Create Ad.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation2/SoftUniBazar/Data/Models/Ad.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+using static SoftUniBazar.Data.Constants.DataConstants.Ad;
+
+namespace SoftUniBazar.Data.Models
+{
+    public class Ad
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLegth)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLegth)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; } = null!;
+
+        [Required]
+        public IdentityUser Owner { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+    }
+}
+
