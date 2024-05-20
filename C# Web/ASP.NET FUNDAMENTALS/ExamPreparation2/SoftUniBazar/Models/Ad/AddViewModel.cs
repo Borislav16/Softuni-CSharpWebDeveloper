2de846b6c87279c549aa7de6d6a5385a5106f4d4
Borislav16 <borko.g16@gmail.com>
2024-05-20 14:24:54 +0300

    Create AddViewModel.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation2/SoftUniBazar/Models/Ad/AddViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SoftUniBazar.Data.Models;
+using SoftUniBazar.Models.Category;
+using System.ComponentModel.DataAnnotations;
+using static SoftUniBazar.Data.Constants.DataConstants.Ad;
+
+namespace SoftUniBazar.Models.Ad
+{
+    public class AddViewModel
+    {
+
+
+        [Required]
+        [StringLength(NameMaxLegth, MinimumLength = NameMinLegth)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLegth, MinimumLength = DescriptionMinLegth)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public List<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+    }
+}
