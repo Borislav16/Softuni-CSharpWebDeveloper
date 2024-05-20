e1c6bb886d9c1ce02a54d1c69a9fa553b6cb8513
Borislav16 <borko.g16@gmail.com>
2024-05-20 14:25:03 +0300

    Create AllViewModel.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/ExamPreparation2/SoftUniBazar/Models/Ad/AllViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static SoftUniBazar.Data.Constants.DataConstants.Ad;
+
+namespace SoftUniBazar.Models.Ad
+{
+    public class AllViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(NameMaxLegth, MinimumLength = NameMinLegth)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLegth, MinimumLength = DescriptionMinLegth)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Owner { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public string Category { get; set; } = null!;
+    }
+}
