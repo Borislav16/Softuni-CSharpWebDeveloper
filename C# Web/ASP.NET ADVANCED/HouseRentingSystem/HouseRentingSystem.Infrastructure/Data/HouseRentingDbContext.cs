8636abcacfc151ceaffd7e542aa5fa5c975d56a3
Borislav16 <borko.g16@gmail.com>
2024-05-24 11:16:50 +0300

    Create HouseRentingDbContext.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/HouseRentingDbContext.cs
@@ -0,0 +1,31 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Models;
+using HouseRentingSystem.Infrastructure.Data.SeedDb;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrasructure.Data
+{
+    public class HouseRentingDbContext : IdentityDbContext
+    {
+        public HouseRentingDbContext(DbContextOptions<HouseRentingDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new AgentConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new HouseConfiguration());
+
+            base.OnModelCreating(builder);
+        }
+        DbSet<Agent> Agents { get; set; } = null!;
+
+        DbSet<Category> Categories { get; set; } = null!;
+
+        DbSet<House> Houses { get; set; } = null!;
+
+    }
+}
