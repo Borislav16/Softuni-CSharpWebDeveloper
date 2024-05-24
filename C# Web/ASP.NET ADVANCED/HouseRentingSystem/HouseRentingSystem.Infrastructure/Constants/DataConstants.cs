d3c187bb08b2955e3c2572f09bc81875733b1ba1
Borislav16 <borko.g16@gmail.com>
2024-05-24 11:16:39 +0300

    Create DataConstants.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace HouseRentingSystem.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int CategoryNameLength = 50;
+
+
+        public const int HouseTitleMinLength = 10;
+        public const int HouseTitleMaxLength = 50;
+
+        public const int HouseAddressMinLength = 30;
+        public const int HouseAddressMaxLength = 150;
+
+        public const int HouseDescriptionMinLength = 50; 
+        public const int HouseDescriptionMaxLength = 500;
+        
+        public const string HousePricePerMonthMin = "0.00";
+        public const string HousePricePerMonthMax = "2000.00";
+
+        public const int AgentPhoneMinLength = 7;
+        public const int AgentPhoneMaxLength = 15;
+    }
+}
