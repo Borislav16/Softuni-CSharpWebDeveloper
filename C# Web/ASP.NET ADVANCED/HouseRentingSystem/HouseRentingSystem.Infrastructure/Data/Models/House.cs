4531aeae992baa8ce98ead2ee46d06ad861b4d94
Borislav16 <borko.g16@gmail.com>
2024-05-24 11:17:18 +0300

    Create House.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Models/House.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    [Comment("House to rent")]
+    public class House
+    {
+        [Key]
+        [Comment("House Identifier")]
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(HouseTitleMaxLength)]
+        [Comment("Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(HouseAddressMaxLength)]
+        [Comment("House address")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(HouseDescriptionMaxLength)]
+        [Comment("House description")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("House image url")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Monthly price")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PricePerMonth { get; set; }
+
+        [Required]
+        [Comment("Category identifier")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Comment("Agent identifier")]
+        public int AgentId { get; set; }
+
+        [Comment("User id of the renterer")]
+        public string? RenterId { get; set; }
+
+        public Category Category { get; set; } = null!;
+
+        public Agent Agent { get; set; } = null!;
+    }
+}
