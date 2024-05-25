e210daa0cecca88ae097cc8be599f6a0b361180b
Borislav16 <borko.g16@gmail.com>
2024-05-25 14:06:55 +0300

    Create HomeController.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HouseRentingSystem.Infrastructure.Data
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = new IndexViewModel();
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
