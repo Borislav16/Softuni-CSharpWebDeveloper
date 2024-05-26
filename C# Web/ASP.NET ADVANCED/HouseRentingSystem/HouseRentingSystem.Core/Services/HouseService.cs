3506b42c87fbf34ed7a451d23ad632066386ecf9
Borislav16 <borko.g16@gmail.com>
2024-05-26 09:59:18 +0300

    Create HouseService.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Services/HouseService.cs
@@ -0,0 +1,75 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.House;
+using HouseRentingSystem.Infrastructure.Data.Common;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Services
+{
+    public class HouseService : IHouseSevice
+    {
+        private readonly IRepository repository;
+
+        public HouseService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<HouseCategoryServiceModel>> AllCategoriesAsync()
+        {
+            return await repository
+                .AllReadOnly<Category>()
+                .Select(c => new HouseCategoryServiceModel
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                })
+                .ToListAsync();
+        }
+
+        public async Task<bool> CategoryExistsAsync(int categoryId)
+        {
+            return await repository.AllReadOnly<Category>()
+                .AnyAsync(c => c.Id == categoryId);
+        }
+
+        public async Task<int> CreateAsync(HouseFormModel model, int agentId)
+        {
+            House house = new House()
+            {
+                Address = model.Address,
+                AgentId = agentId,
+                CategoryId = model.CategoryId,
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+                PricePerMonth = model.PricePerMonth,
+                Title = model.Title,
+            };
+
+            await repository.AddAsync(house);
+            await repository.SaveChangesAsync();
+
+            return house.Id;
+        }
+
+        public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync()
+        {
+            return await repository
+                .AllReadOnly<Infrastructure.Data.Models.House>()
+                .OrderByDescending(h => h.Id)
+                .Take(3)
+                .Select(h => new HouseIndexServiceModel
+                {
+                    Id = h.Id,
+                    Title = h.Title,
+                    ImageUrl = h.ImageUrl,
+                })
+                .ToListAsync();
+        }
+    }
+}
