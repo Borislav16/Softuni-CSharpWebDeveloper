bf06c1f4c1a685ffb64e95f8c701c555eea80400
Borislav16 <borko.g16@gmail.com>
2024-05-26 09:58:26 +0300

    Update BecomeAgentFormModel.cs

--- C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
 
 namespace HouseRentingSystem.Core.Models.Agent
 {
     public class BecomeAgentFormModel
     {
+        [Required]
+        [StringLength(AgentPhoneMaxLength,
+             MinimumLength = AgentPhoneMinLength,
+            ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
     }
 }
