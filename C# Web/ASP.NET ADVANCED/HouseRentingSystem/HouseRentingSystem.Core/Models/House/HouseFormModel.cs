7a1b38ff96675ee1bde951335726e61410b15b26
Borislav16 <borko.g16@gmail.com>
2024-05-26 09:58:48 +0300

    Update HouseFormModel.cs

--- C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseFormModel.cs
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseFormModel.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
 
 namespace HouseRentingSystem.Core.Models.House
 {
     public class HouseFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseTitleMaxLength,
+                    MinimumLength = HouseTitleMinLength,
+                    ErrorMessage = LengthMessage)]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseAddressMaxLength,
+            MinimumLength = HouseAddressMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Address { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseDescriptionMaxLength,
+            MinimumLength = HouseDescriptionMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Description { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Range(typeof(decimal),
+            HousePricePerMonthMin,
+            HousePricePerMonthMax,
+            ConvertValueInInvariantCulture = true,
+            ErrorMessage = "Price per month must be a positive number and less than {2} leva")]
+        [Display(Name = "Price Per Month")]
+        public decimal PricePerMonth { get; set; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<HouseCategoryServiceModel> Categories { get; set; } =
+            new List<HouseCategoryServiceModel>();
+
     }
 }
