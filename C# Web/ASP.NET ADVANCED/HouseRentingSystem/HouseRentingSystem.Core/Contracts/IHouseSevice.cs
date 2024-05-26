a3a91b7c31f233f610a99841cedad8bf80dfcf4a
Borislav16 <borko.g16@gmail.com>
2024-05-26 09:58:15 +0300

    Create IHouseSevice.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Contracts/IHouseSevice.cs
@@ -0,0 +1,20 @@
+﻿using HouseRentingSystem.Core.Models.House;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Contracts
+{
+    public interface IHouseSevice
+    {
+        Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync();
+
+        Task<IEnumerable<HouseCategoryServiceModel>> AllCategoriesAsync();
+
+        Task<bool> CategoryExistsAsync(int categoryId);
+
+        Task<int> CreateAsync(HouseFormModel model, int agentId);
+    }
+}
