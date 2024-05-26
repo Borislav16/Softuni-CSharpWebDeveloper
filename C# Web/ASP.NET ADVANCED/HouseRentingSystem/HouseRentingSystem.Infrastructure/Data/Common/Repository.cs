d39f018f0b057fa9406295f99bb37ba572eef793
Borislav16 <borko.g16@gmail.com>
2024-05-26 09:59:47 +0300

    Create Repository.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,46 @@
+﻿using HouseRentingSystem.Infrasructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(HouseRentingDbContext _context)
+        {
+                context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class 
+        {
+            return context.Set<T>();
+        }
+ 
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+    }
+}
