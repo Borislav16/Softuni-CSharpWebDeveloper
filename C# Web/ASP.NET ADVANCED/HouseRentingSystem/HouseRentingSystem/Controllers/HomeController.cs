80ddc0009ce39f485ffef43320c01a26ba81fb3a
Borislav16 <borko.g16@gmail.com>
2024-05-26 10:01:22 +0300

    Update HomeController.cs

--- C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -1,20 +1,31 @@
-﻿using HouseRentingSystem.Core.Models.Home;
+﻿using HouseRentingSystem.Controllers;
+using HouseRentingSystem.Core.Contracts;
 using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace HouseRentingSystem.Infrastructure.Data
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        private readonly IHouseSevice houseSevice;
+
+        public HomeController(IHouseSevice _houseService)
+        {
+            houseSevice = _houseService;
+        }
+
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
-            var model = new IndexViewModel();
+            var model = await houseSevice.LastThreeHousesAsync();
 
             return View(model);
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
