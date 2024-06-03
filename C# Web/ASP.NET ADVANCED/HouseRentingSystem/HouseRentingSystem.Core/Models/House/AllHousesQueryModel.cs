2a549688471a2e5da7bb09e7f59bd490d63458ac
Borislav16 <borko.g16@gmail.com>
2024-06-03 14:04:43 +0300

    Update AllHousesQueryModel.cs

--- C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Models/House/AllHousesQueryModel.cs
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Models/House/AllHousesQueryModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HouseRentingSystem.Core.Enumerations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,23 @@ namespace HouseRentingSystem.Core.Models.House
 {
     public class AllHousesQueryModel
     {
+        public int HousesPerPage { get; } = 3;
+
+        public string Category { get; set; } = null!;
+
+        [Display(Name = "Search by text")]
+        public string SearchTerm { get; set; } = null!;
+
+        public HouseSorting Sorting { get; set; }
+
+        public int CurrentPage { get; set; } = 1;
+
+        public int TotalHousesCount { get; set; }
+
+        public IEnumerable<string> Categories { get; set; } = null!;
+
+        public IEnumerable<HouseServiceModel> Houses { get; set; }
+            = new List<HouseServiceModel>();
+
     }
 }
