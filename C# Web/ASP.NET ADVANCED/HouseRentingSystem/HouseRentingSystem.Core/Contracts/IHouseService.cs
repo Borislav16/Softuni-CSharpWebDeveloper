43389bb31c67c25fc5dd82c82b44f6309b394f4a
Borislav16 <borko.g16@gmail.com>
2024-06-04 15:34:17 +0300

    Create IHouseService.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Contracts/IHouseService.cs
@@ -0,0 +1,54 @@
+﻿using HouseRentingSystem.Core.Enumerations;
+using HouseRentingSystem.Core.Models.House;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Contracts
+{
+    public interface IHouseService
+    {
+        Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync();
+
+        Task<IEnumerable<HouseCategoryServiceModel>> AllCategoriesAsync();
+
+        Task<bool> CategoryExistsAsync(int categoryId);
+
+        Task<int> CreateAsync(HouseFormModel model, int agentId);
+
+        Task<HouseQueryServiceModel> AllAsync(
+            string? category = null,
+            string? searchTerm = null,
+            HouseSorting sorting = HouseSorting.Newest,
+            int currentPage = 1,
+            int housesPerPage = 1);
+
+        Task<IEnumerable<string>> AllCategoriesNamesAsync();
+
+        Task<IEnumerable<HouseServiceModel>> AllHousesByAgentId(int agentId);
+
+        Task<IEnumerable<HouseServiceModel>> AllHousesByUserId(string UserId);
+
+        Task<bool> ExistsAsync(int id);
+
+        Task<HouseDetailsServiceModel> HouseDetailsByIdAsync(int id);
+
+        Task EditAsync(int houseId, HouseFormModel model);
+
+        Task<bool> HasAgentWithIdAsync(int houseId, string userId);
+
+        Task<HouseFormModel?> GetHouseFormModelByIdAsync(int id);
+
+        Task DeleteAsync(int houseId);
+
+        Task<bool> IsRentedAsync(int houseId);
+
+        Task<bool> IsRentedByUserWithIdAsync(int houseId, string userId);
+
+        Task RentAsync(int id, string userId);
+
+        Task LeaveAsync(int houseId, string userId);
+    }
+}
