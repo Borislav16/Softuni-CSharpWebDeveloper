cb1eaabdc06cde5618ffdbb32e810a9e0cedd47a
Borislav16 <borko.g16@gmail.com>
2024-06-06 11:47:34 +0300

    Create IRentService.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Core/Contracts/IRentService.cs
@@ -0,0 +1,14 @@
+﻿using HouseRentingSystem.Core.Models.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Core.Contracts
+{
+    public interface IRentService
+    {
+        Task<IEnumerable<RentServiceModel>> AllAsync();
+    }
+}
