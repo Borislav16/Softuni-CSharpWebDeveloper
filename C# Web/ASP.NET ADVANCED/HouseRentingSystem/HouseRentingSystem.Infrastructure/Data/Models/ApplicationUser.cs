a3474a1bc4d410b3c29a705dba51ffd2daf143e2
Borislav16 <borko.g16@gmail.com>
2024-06-06 11:50:22 +0300

    Create ApplicationUser.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(UserFirstNameMaxLength)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(UserLastNameMaxLength)]
+        public string LastName { get; set; } = string.Empty;
+
+        public Agent? Agent { get; set; }
+    }
+}
