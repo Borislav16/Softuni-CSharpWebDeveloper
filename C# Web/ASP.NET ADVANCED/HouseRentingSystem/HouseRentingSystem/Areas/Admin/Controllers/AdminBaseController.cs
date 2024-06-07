6f3bc53da916ef4caca6af525ca67731cc01bd1c
Borislav16 <borko.g16@gmail.com>
2024-06-07 12:47:21 +0300

    Create AdminBaseController.cs

---
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static HouseRentingSystem.Core.Constants.AdministratorConstants;
+
+namespace HouseRentingSystem.Areas.Admin.Controllers
+{
+    [Area(AdminAreaName)]
+    [Authorize(Roles = AdminRole)]
+    public class AdminBaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
