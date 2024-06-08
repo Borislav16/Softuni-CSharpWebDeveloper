b3897f41886307a54057e3bf58b559fc96f33bd1
Borislav16 <borko.g16@gmail.com>
2024-06-08 11:07:39 +0300

    Update ClaimPrincipalExtensions.cs

--- C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem/Extensions/ClaimPrincipalExtensions.cs
+++ C# Web/ASP.NET ADVANCED/HouseRentingSystem/HouseRentingSystem/Extensions/ClaimPrincipalExtensions.cs
@@ -1,10 +1,16 @@
-﻿namespace System.Security.Claims
+﻿using static HouseRentingSystem.Core.Constants.AdministratorConstants;
+namespace System.Security.Claims
 {
     public static class ClaimPrincipalExtensions
     {
         public static string Id(this ClaimsPrincipal user)
         {
             return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRole);
         }
     }
 }
