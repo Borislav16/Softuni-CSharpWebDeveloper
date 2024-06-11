8745f2eecbc613829670e34dc021c1508d953cc6
Borislav16 <borko.g16@gmail.com>
2024-06-11 12:27:53 +0300

    Create ProductsData.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/MVCIntroDemo/MVCIntroDemo/Seeding/ProductsData.cs
@@ -0,0 +1,31 @@
+﻿using MVCIntroDemo.Models.Product;
+
+namespace MVCIntroDemo.Seeding
+{
+    public static class ProductsData
+    {
+        public static IEnumerable<ProductViewModel> Products =
+            new List<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Id = 1,
+                    Name = "Cheese",
+                    Price = 7.00
+                },
+                new ProductViewModel()
+                {
+                    Id = 2,
+                    Name = "Ham",
+                    Price = 5.50
+                },
+                new ProductViewModel() 
+                {
+                    Id=3,
+                    Name = "Bread",
+                    Price = 1.50
+                }
+            };
+
+    }
+}
