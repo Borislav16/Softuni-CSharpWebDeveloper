2f7eb599e700b81dbe60784f2b3f96df185dc1de
Borislav16 <borko.g16@gmail.com>
2024-06-14 10:56:27 +0300

    Create HomeController.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TaskBoardApp.Data;
+using TaskBoardApp.Models.Home;
+
+namespace TaskBoardApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TaskBoardDbContext data;
+
+        public HomeController(TaskBoardDbContext context)
+        {
+            this.data = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> taskBoards = this.data.Boards.Select(b => b.Name).Distinct().ToList();
+            var tasksCount = new List<HomeBoardModel>();
+            foreach (var boardName in taskBoards)
+            {
+                int tasksInBoard = this.data.Tasks.Where(t => t.Board.Name == boardName).Count();
+                tasksCount.Add(new HomeBoardModel
+                {
+                    BoardName = boardName,
+                    TasksCount = tasksInBoard
+                });
+            }
+
+            var userTasksCount = -1;
+
+            if (this.User.Identity.IsAuthenticated)
+            {
+                var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                userTasksCount = this.data.Tasks.Where(t => t.OwnerId == currentUserId).Count();
+            }
+
+            var homeModel = new HomeViewModel
+            {
+                AllTasksCount = this.data.Tasks.Count(),
+                BoardsWithTasksCount = tasksCount,
+                UserTasksCount = userTasksCount
+            };
+
+            return View(homeModel);
+        }
+    }
+}
