381187966044f6d47a39bdfefdb51303b61df708
Borislav16 <borko.g16@gmail.com>
2024-06-15 10:31:07 +0300

    Create TaskViewModel.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/TaskBoardApp/TaskBoardApp/Models/Task/TaskViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.TaskConstants;
+
+namespace TaskBoardApp.Models.Task
+{
+    public class TaskViewModel
+    {
+
+        public int Id { get; init; }
+
+        public string Title { get; init; } = null!;
+
+        public string Description { get; init; } = null!;
+
+        public string Owner { get; init; } = null!;
+    }
+}
