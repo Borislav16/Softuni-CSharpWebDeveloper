77f8a61c08ad76a330c25f011acf097eb1b4d5d8
Borislav16 <borko.g16@gmail.com>
2024-06-15 10:22:14 +0300

    Create BoardViewModel.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/TaskBoardApp/TaskBoardApp/Models/Board/BoardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskBoardApp.Models.Task;
+using static TaskBoardApp.Data.DataConstants.Board;
+
+namespace TaskBoardApp.Models.Board
+{
+    public class BoardViewModel
+    {
+        
+        public int Id { get; init; }
+
+        public string Name { get; init; } = null!;
+
+        public IEnumerable<TaskViewModel> Tasks { get; set; } = new List<TaskViewModel>();
+    }
+}
