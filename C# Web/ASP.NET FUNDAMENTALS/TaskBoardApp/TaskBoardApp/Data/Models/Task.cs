f5e751feecef067b43a607cc5c3361edfeb9c9d9
Borislav16 <borko.g16@gmail.com>
2024-06-15 10:21:56 +0300

    Create Task.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/TaskBoardApp/TaskBoardApp/Data/Models/Task.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static TaskBoardApp.Data.DataConstants.TaskConstants;
+
+namespace TaskBoardApp.Data.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TaskMaxTitle)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(TaskMaxDescription)]
+        public string Description { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public int BoardId { get; set; }
+
+        [ForeignKey(nameof(BoardId))]
+        public Board Board { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; } = null!;
+
+        [ForeignKey(nameof(OwnerId))]
+        public IdentityUser User { get; set; } = null!;
+
+    }
+}
