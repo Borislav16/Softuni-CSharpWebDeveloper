e584b1ff52f33aa824afbe36ed4670d21fcc8f81
Borislav16 <borko.g16@gmail.com>
2024-06-15 10:30:59 +0300

    Create TaskFormModel.cs

---
+++ C# Web/ASP.NET FUNDAMENTALS/TaskBoardApp/TaskBoardApp/Models/Task/TaskFormModel.cs
@@ -0,0 +1,24 @@
+﻿using NuGet.Common;
+using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.TaskConstants;
+
+namespace TaskBoardApp.Models.Task
+{
+    public class TaskFormModel
+    {
+        [Required]
+        [StringLength(TaskMaxTitle, MinimumLength = TaskMinTitle,
+            ErrorMessage = "Title should be at least {2} characters long.")]
+        public string Title { get; init; } = null!;
+
+        [Required]
+        [StringLength(TaskMaxDescription, MinimumLength = TaskMinDescription,
+            ErrorMessage = "Description should be at least {2} characters long.")]
+        public string Description { get; init; } = null!;
+
+        [Display(Name = "Board")]
+        public int BoardId { get; init; }
+
+        public IEnumerable<TaskBoardModel>? Boards { get; set; }
+    }
+}
