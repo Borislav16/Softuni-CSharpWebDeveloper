30ec8aca1cee170f6f219bdf145b8a760750bc3d
Borislav16 <borko.g16@gmail.com>
2024-07-08 11:41:24 +0300

    Create 06.SumDigits.js

---
+++ JS Front-End/JS FRONT-END/04.ExerciseJSSyntaxFundamentals/06.SumDigits.js
@@ -0,0 +1,10 @@
+function sumDigits(number){
+    const digitsString = number.toString();
+    let sum = 0;
+
+    for (let i = 0; i < digitsString.length; i++) {
+        sum += Number(digitsString[i]);
+    }
+
+    console.log(sum);
+}
