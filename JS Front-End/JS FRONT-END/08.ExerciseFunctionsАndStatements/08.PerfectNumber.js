4da437efb12b8f6bbe0b09d59e0859b431366933
Borislav16 <borko.g16@gmail.com>
2024-07-09 09:46:20 +0300

    Create 08.PerfectNumber.js

---
+++ JS Front-End/JS FRONT-END/08.ExerciseFunctionsАndStatements/08.PerfectNumber.js
@@ -0,0 +1,17 @@
+function perfectNumber(number) {
+    let divisorsArr = [];
+    for (let i = 0; i < number; i++) {
+        if (number%i==0){
+            divisorsArr.push(i);
+        }
+    }
+    let sum = 0;
+    for (const divisorsArrElement of divisorsArr) {
+        sum+=divisorsArrElement;
+    }
+    if (sum===number){
+        console.log("We have a perfect number!")
+    } else {
+        console.log("It's not so perfect.")
+    }
+}
