2550a7983d7e6628bf3235d089c5c5fcef225961
Borislav16 <borko.g16@gmail.com>
2024-07-09 09:44:55 +0300

    Create 06.SignCheck.js

---
+++ JS Front-End/JS FRONT-END/07.FunctionsАndStatements/06.SignCheck.js
@@ -0,0 +1,9 @@
+function signCheck(numOne, numTwo, numThree){
+    if((numOne * numTwo * numThree) >= 0){
+        console.log("Positive")
+    }
+    else{
+        console.log("Negative")
+
+    }
+}
