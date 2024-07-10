5e81237c50821e54372db25eeb3dbde2e083387e
Borislav16 <borko.g16@gmail.com>
2024-07-10 10:18:21 +0300

    Create 09.LoadingBar.js

---
+++ JS Front-End/JS FRONT-END/08.ExerciseFunctionsАndStatements/09.LoadingBar.js
@@ -0,0 +1,17 @@
+function loadingBar(number){
+    let bar="";
+    for (let i = 1; i <= 100; i+=10) {
+        if (i<number){
+            bar+="%";
+        } else {
+            bar+=".";
+        }
+    }
+    if (number===100){
+        console.log("100% Complete!")
+        console.log(bar)
+    } else {
+        console.log(`${number}% [${bar}]`)
+        console.log("Still loading...")
+    }
+}
