1b4f966975068b3246fa7784a819d6794de88bc6
Borislav16 <borko.g16@gmail.com>
2024-07-11 14:52:40 +0300

    Create SumNumbers.js

---
+++ JS Front-End/JS FRONT-END/11.DOMIntroduction/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,7 @@
+function calc() {
+    let first = document.getElementById("num1").value;
+    let second = document.getElementById("num2").value;
+    let result = Number(first) + Number(second);
+    document.getElementById("sum").value = result;
+    
+}
