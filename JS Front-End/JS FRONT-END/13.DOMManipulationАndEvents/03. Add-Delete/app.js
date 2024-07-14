a6cc51282775656f0598e5f585c9ee3e2caf6e95
Borislav16 <borko.g16@gmail.com>
2024-07-14 10:13:03 +0300

    Create app.js

---
+++ JS Front-End/JS FRONT-END/13.DOMManipulationАndEvents/03. Add-Delete/app.js
@@ -0,0 +1,14 @@
+function addItem() {
+    debugger;
+    let newItemText = document.querySelector("#newItemText").value;
+    let newLi = document.createElement("li");
+    newLi.textContent = newItemText;
+    let deleteAnchor = document.createElement("a");
+    deleteAnchor.href = "#";
+    deleteAnchor.text = "[Delete]";
+    deleteAnchor.addEventListener("click", () => {
+      deleteAnchor.parentElement.remove();
+    });
+    newLi.appendChild(deleteAnchor);
+    document.querySelector("#items").appendChild(newLi);
+  }
