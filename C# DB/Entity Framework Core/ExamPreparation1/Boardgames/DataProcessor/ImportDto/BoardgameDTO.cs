768ecbf6f58471973f4a472e8b64cf6bdde9aedb
Borislav16 <borko.g16@gmail.com>
2024-08-04 12:28:23 +0300

    Create BoardgameDTO.cs

---
+++ C# DB/Entity Framework Core/ExamPreparation1/Boardgames/DataProcessor/ImportDto/BoardgameDTO.cs
@@ -0,0 +1,28 @@
+﻿using Boardgames.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    [XmlType("Boardgame")]
+    public class BoardgameDTO
+    {
+        [Required]
+        [MaxLength(20), MinLength(10)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 10.00)]
+        public double Rating { get; set; }
+
+        [Required]
+        [Range(2018, 2023)]
+        public int YearPublished { get; set; }
+
+        [Required]
+        public int CategoryType { get; set; }
+
+        [Required]
+        public string Mechanics { get; set; }
+    }
+}
