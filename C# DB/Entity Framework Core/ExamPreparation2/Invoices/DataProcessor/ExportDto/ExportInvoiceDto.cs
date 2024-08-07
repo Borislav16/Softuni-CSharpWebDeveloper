571da5d7bb3414b74dc30641c24fdf678f4959f9
Borislav16 <borko.g16@gmail.com>
2024-08-07 10:37:19 +0300

    Create ExportInvoiceDto.cs

---
+++ C# DB/Entity Framework Core/ExamPreparation2/Invoices/DataProcessor/ExportDto/ExportInvoiceDto.cs
@@ -0,0 +1,22 @@
+﻿using Invoices.Data.Models.Enums;
+using Microsoft.VisualBasic;
+using System.Xml.Serialization;
+
+namespace Invoices.DataProcessor.ExportDto
+{
+    [XmlType("Invoice")]
+    public class ExportInvoiceDto
+    {
+        [XmlElement("InvoiceNumber")]
+        public int InvoiceNumber { get; set; }
+
+        [XmlElement("InvoiceAmount")]
+        public decimal InvoiceAmount { get; set; }
+
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; } = null!;
+
+        [XmlElement("Currency")]
+        public CurrencyType CurrencyType { get; set; }
+    }
+}
