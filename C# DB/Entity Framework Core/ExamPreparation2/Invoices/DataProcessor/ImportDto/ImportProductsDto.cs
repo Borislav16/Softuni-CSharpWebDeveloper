76b012be7af867b518a6926aa23bd86875e7603b
Borislav16 <borko.g16@gmail.com>
2024-08-07 10:37:34 +0300

    Create ImportProductsDto.cs

---
+++ C# DB/Entity Framework Core/ExamPreparation2/Invoices/DataProcessor/ImportDto/ImportProductsDto.cs
@@ -0,0 +1,30 @@
+﻿using Invoices.Data.Models.Enums;
+using Invoices.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    public class ImportProductsDto
+    {
+        [Required]
+        [MaxLength(30), MinLength(9)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(5.00, 1000.00)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        public CategoryType CategoryType { get; set; }
+
+        [JsonProperty("Clients")]
+        public int[] ClientId { get; set; }
+    }
+}
