77878e9fa33d97a8ca56eb28fb350a534424e3fa
Borislav16 <borko.g16@gmail.com>
2024-08-07 10:37:36 +0300

    Create Serializer.cs

---
+++ C# DB/Entity Framework Core/ExamPreparation2/Invoices/DataProcessor/Serializer.cs
@@ -0,0 +1,67 @@
+﻿namespace Invoices.DataProcessor
+{
+    using Boardgames.Helper;
+    using Invoices.Data;
+    using Invoices.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System.Globalization;
+
+    public class Serializer
+    {
+        public static string ExportClientsWithTheirInvoices(InvoicesContext context, DateTime date)
+        {
+            var clients = context.Clients
+                .Where(c => c.Invoices.Any(i => i.IssueDate > date))
+                .Select(c => new ExportClientDto()
+                {
+                    Name = c.Name,
+                    VatNumber = c.NumberVat,
+                    InvoicesCount = c.Invoices.Count,
+                    Invoices = c.Invoices
+                        .OrderBy(i => i.IssueDate)
+                        .ThenByDescending(i => i.DueDate)
+                        .Select(i => new ExportInvoiceDto()
+                        {
+                            InvoiceNumber = i.Number,
+                            InvoiceAmount = i.Amount,
+                            DueDate = i.DueDate.ToString("MM/dd/yyyy"),
+                            CurrencyType = i.CurrencyType
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.Invoices.Count())
+                .ThenBy(c => c.Name)
+                .ToList();
+
+            return XmlSerializationHelper.Serialize(clients, "Clients");
+        }
+
+        public static string ExportProductsWithMostClients(InvoicesContext context, int nameLength)
+        {
+            var products = context.Products
+                .Where(p => p.ProductsClients.Any(pc => pc.Client.Name.Length >= nameLength))
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Price,
+                    Category = p.CategoryType.ToString(),
+                    Clients = p.ProductsClients
+                        .Where(pc => pc.Client.Name.Length >= nameLength)
+                        .Select(pc => new
+                        {
+                            pc.Client.Name,
+                            pc.Client.NumberVat
+                        })
+                        .OrderBy(pc => pc.Name)
+                        .ToList()
+                })
+                .OrderByDescending(p => p.Clients.Count())
+                .ThenBy(p => p.Name)
+                .Take(5)
+                .ToList();
+
+            return JsonConvert.SerializeObject(products, Formatting.Indented);
+            
+        }
+    }
+}
