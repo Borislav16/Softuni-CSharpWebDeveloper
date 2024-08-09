86c5b4d3ecd4572cf37fff89e965cf01969dba76
Borislav16 <borko.g16@gmail.com>
2024-08-09 13:16:10 +0300

    Create ImportPatientDto.cs

---
+++ C# DB/Entity Framework Core/ExamPreparation3/Medicines/DataProcessor/ImportDtos/ImportPatientDto.cs
@@ -0,0 +1,29 @@
+﻿using Medicines.Data.Models.Enums;
+using Medicines.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Medicines.DataProcessor.ImportDtos
+{
+    public class ImportPatientDto
+    {
+        [Required]
+        [MaxLength(100), MinLength(5)]
+        public string FullName { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        public int AgeGroup { get; set; }
+
+        [Required]
+        [Range(0, 1)]
+        public Gender Gender { get; set; }
+
+        public int[] Medicines { get; set; }
+    }
+}
