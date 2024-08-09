fa3aa2c29f7c805fae805641240ec35f7ce2c2f5
Borislav16 <borko.g16@gmail.com>
2024-08-09 13:16:08 +0300

    Create ImportMedicineDto.cs

---
+++ C# DB/Entity Framework Core/ExamPreparation3/Medicines/DataProcessor/ImportDtos/ImportMedicineDto.cs
@@ -0,0 +1,33 @@
+﻿using Medicines.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Medicines.DataProcessor.ImportDtos
+{
+    [XmlType("Medicine")]
+    public class ImportMedicineDto
+    {
+        [Required]
+        [MaxLength(150), MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, 1000.00)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [XmlAttribute("category")]
+        [Range(0, 4)]
+        public int Category { get; set; }
+
+        [Required]
+        public string ProductionDate { get; set; }
+
+        [Required]
+        public string ExpiryDate { get; set; }
+
+        [Required]
+        [MaxLength(100), MinLength(3)]
+        public string Producer { get; set; }
+    }
+}
