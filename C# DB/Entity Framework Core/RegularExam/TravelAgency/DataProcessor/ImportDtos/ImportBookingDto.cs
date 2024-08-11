d39c7748c1bdcaef5202bb5119ef3068a0123314
Borislav16 <borko.g16@gmail.com>
2024-08-11 13:15:10 +0300

    Create ImportBookingDto.cs

---
+++ C# DB/Entity Framework Core/RegularExam/TravelAgency/DataProcessor/ImportDtos/ImportBookingDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Data.Models;
+
+namespace TravelAgency.DataProcessor.ImportDtos
+{
+    public class ImportBookingDto
+    {
+        [Required]
+        public string BookingDate { get; set; }
+
+        [Required]
+        public string CustomerName { get; set; }
+
+        [Required]
+        public string TourPackageName { get; set; }
+    }
+}
