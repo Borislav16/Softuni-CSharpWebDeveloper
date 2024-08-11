de34249f55718e046ca883ed2ddf62cf5cf79037
Borislav16 <borko.g16@gmail.com>
2024-08-11 13:15:05 +0300

    Create ExportGuideDto.cs

---
+++ C# DB/Entity Framework Core/RegularExam/TravelAgency/DataProcessor/ExportDtos/ExportGuideDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TravelAgency.DataProcessor.ExportDtos
+{
+    [XmlType("Guide")]
+    public class ExportGuideDto
+    {
+        [XmlElement("FullName")]
+        public string FullName { get; set; }
+
+        [XmlArray("TourPackages")]
+        public ExportTourPackageDto[] TourPackages { get; set; }
+    }
+}
