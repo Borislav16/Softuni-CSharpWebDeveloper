2b344fdd24533006fc954d5a141bd4a2fb12fefe
Borislav16 <borko.g16@gmail.com>
2024-08-11 13:15:07 +0300

    Create ExportTourPackageDto.cs

---
+++ C# DB/Entity Framework Core/RegularExam/TravelAgency/DataProcessor/ExportDtos/ExportTourPackageDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace TravelAgency.DataProcessor.ExportDtos
+{
+    [XmlType("TourPackage")]
+    public class ExportTourPackageDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Description")]
+        public string Description { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+    }
+}
