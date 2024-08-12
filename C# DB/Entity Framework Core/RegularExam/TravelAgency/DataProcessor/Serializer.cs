1cdc028bbc80b8ce2291fbb18a56d1e4ac29cb86
Borislav16 <borko.g16@gmail.com>
2024-08-12 18:04:15 +0300

    Create Serializer.cs

---
+++ C# DB/Entity Framework Core/RegularExam/TravelAgency/DataProcessor/Serializer.cs
@@ -0,0 +1,61 @@
+﻿using Boardgames.Helper;
+using Newtonsoft.Json;
+using TravelAgency.Data;
+using TravelAgency.Data.Models;
+using TravelAgency.DataProcessor.ExportDtos;
+
+namespace TravelAgency.DataProcessor
+{
+    public class Serializer
+    {
+        public static string ExportGuidesWithSpanishLanguageWithAllTheirTourPackages(TravelAgencyContext context)
+        {
+            var guides = context.Guides
+                .Where(g => g.Language == (Language)3)
+                .Select(g => new ExportGuideDto()
+                {
+                    FullName = g.FullName,
+                    TourPackages = g.TourPackagesGuides
+                        .Select(tpg => new ExportTourPackageDto()
+                        {
+                            Name = tpg.TourPackage.PackageName,
+                            Description = tpg.TourPackage.Description,
+                            Price = tpg.TourPackage.Price,
+                        })
+                        .OrderByDescending(tp => tp.Price)
+                        .ThenBy(tp => tp.Name)
+                        .ToArray(),
+                })
+                .OrderByDescending(g => g.TourPackages.Count())
+                .ThenBy(g => g.FullName)
+                .ToList();
+
+            return XmlSerializationHelper.Serialize(guides, "Guides");
+        }
+
+        public static string ExportCustomersThatHaveBookedHorseRidingTourPackage(TravelAgencyContext context)
+        {
+            var customers = context.Customers
+                .Where(c => c.Bookings.Any(b => b.TourPackage.PackageName == "Horse Riding Tour"))
+                .Select(c => new
+                {
+                    c.FullName,
+                    c.PhoneNumber,
+                    Bookings = c.Bookings
+                        .Where(b => b.TourPackage.PackageName == "Horse Riding Tour")
+                        .OrderBy(b => b.BookingDate)
+                        .Select(b => new
+                        {
+                            TourPackageName = b.TourPackage.PackageName,
+                            Date = b.BookingDate.ToString("yyyy-MM-dd"),
+                        })
+                        .ToList(),
+                })
+                .OrderByDescending(c => c.Bookings.Count())
+                .ThenBy(c => c.FullName)
+                .ToList();
+
+            return JsonConvert.SerializeObject(customers, Formatting.Indented);
+        }
+    }
+}
